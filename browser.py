1eabfe5a59b1adf544ab1d0f2f1578612e31aca9
Conlan Rios <conlan@gmail.com>
2023-12-21 18:18:57 -0500

    Add max attempts for waiting for an image

--- browser.py
+++ browser.py
@@ -13,7 +13,7 @@ class BrowserController:
         # Open a new incognito tab so we can get a fresh game
         self.keyboard.KeyDown('n', True, True, self.pid)
         # Wait for tab to open
-        time.sleep(0.1)
+        time.sleep(0.5)
         # Input URL
         self.keyboard.Type(url, self.pid)
         # Hit return        
@@ -39,31 +39,40 @@ class BrowserController:
         img = pyautogui.screenshot(region=dimensions)
         img.show()
 
-    def waitForButtonAndClick(self, button_image):
+    def waitForButtonAndClick(self, button_image, max_attempts=-1):
         isLookingForButton = True
 
         button_x = None
         button_y = None
 
+        num_attempts = 0
+
         while (isLookingForButton):
             try:
+                num_attempts += 1
+                
                 button_x, button_y = pyautogui.locateCenterOnScreen(button_image)
 
                 print("Found play button at %d, %d" % (button_x, button_y))
                 isLookingForButton = False
             except pyautogui.ImageNotFoundException:
-                print('%s. Will try again...' % button_image)
-                time.sleep(0.25)
+                if (max_attempts > 0) and (num_attempts >= max_attempts):
+                    print("%d Couldn't find %s. Giving up!" % (num_attempts, button_image))
+                else:
+                    print("%d Couldn't find %s. Will try again..." % (num_attempts, button_image))
+
+                    time.sleep(0.25)
         
-        pyautogui.moveTo(button_x // 2, button_y // 2, duration=0.25, tween=pyautogui.easeInOutQuad)
-        pyautogui.click()
+        if not isLookingForButton:
+            pyautogui.moveTo(button_x // 2, button_y // 2, duration=0.25, tween=pyautogui.easeInOutQuad)
+            pyautogui.click()
 
     def startGame(self):
+        # click play button to start
         self.waitForButtonAndClick('assets/play-button.png')
-
-        self.waitForButtonAndClick('assets/news-you-want-x.png')
-
+        # dismiss marketing popup
+        self.waitForButtonAndClick('assets/news-you-want-x.png', 10)
+        # dismiss how to play popup
         self.waitForButtonAndClick('assets/how-to-play-x.png')
-
         # move mouse of the way for CV to detect pieces
-        pyautogui.moveTo(50, 50)
+        pyautogui.moveTo(50, 50, duration=0.25)
